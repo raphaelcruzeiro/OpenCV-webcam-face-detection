cdd4cd058569be0d14c991802808a5bb88ed1ea2
Raphael Cruzeiro <raphaelcruzeiro@raphaelcruzeiro.com>
2012-01-04 19:00:52 -0200

    Initial Commit

---
+++ OpenCV_Test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  OpenCV_Test
+//
+//  Created by Raphael Cruzeiro on 1/4/12.
+//  Copyright (c) 2012 Inspira Tecnologia e Mkt. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
